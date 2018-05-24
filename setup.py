503dfaa4a3476dce7752e8d006aef062ecdc6df5
osaizar <oierlas@gmail.com>
2018-05-24 15:53:02 +0200

    shell interface

--- setup.py
+++ setup.py
@@ -1,5 +1,45 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+from config_hostapd import main as config_hostapd
+from config_bridge import main as config_bridge
+from config_routing import main as config_routing
+
+from commands import getoutput as run_command
+
 def main():
-  print 'TODO: todo todito todo'
-  
+    try:
+        while True:
+          cmd = raw_input("shell > ")
+          run(cmd)
+    except KeyboardInterrupt:
+        end()
+
+def run(cmd):
+    if cmd in ["ip","ifconfig","ipconfig"]:
+        print run_command("ifconfig")
+    elif cmd == "config-wifi":
+        if "y" in raw_input("enable wifi router mode? (y/n)"):
+            config_hostapd()
+    elif cmd == "config-routing":
+        if "y" in raw_input("enable routing? (y/n)"):
+            config_routing()
+    elif cmd == "config-bridge":
+        if "y" in raw_input("enable bridge? (y/n)"):
+            config_bridge()
+    elif "run" in cmd:
+        cmd = cmd.split("run ")[1]
+        print run_command(cmd)
+    elif cmd == "exit":
+        end()
+    else:
+        print "Ez da komandoa topatu"
+
+def end():
+    print "[+] Ayo!"
+    sys.exit()
+
 if __name__ == "__main__":
   main()
